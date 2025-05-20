175f313014cc1e9037156ffc2e95ff155181771a
arunmohanty55 <arunmohanty9535@gmail.com>
2025-05-21 00:11:44 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "public_key" {
+  description = "SSH public key for EC2"
+  type        = string
+}
