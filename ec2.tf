92c9d9b0ca2d74103de2d0608c5940cf824dd78e
arunmohanty55 <arunmohanty9535@gmail.com>
2025-05-21 13:37:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("/home/ec2-user/.ssh/deployer-key.pub")
+  public_key = file("/deployer-key.pub")
 }
 
 resource "aws_instance" "web" {
