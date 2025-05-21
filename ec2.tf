ce57fee3eaf0e5419910cff92729e9ed22408e5b
arunmohanty55 <arunmohanty9535@gmail.com>
2025-05-21 16:31:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,8 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file(/home/ec2-user/.ssh/deployer-key.pub)
-}
 resource "aws_instance" "web" {
   ami           = "ami-0953476d60561c955"  # Amazon Linux 2 AMI (update as needed)
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = "server"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
