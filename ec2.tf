976765b9d203db38ce6f92d53c0d4614f139e2e0
arunmohanty55 <arunmohanty9535@gmail.com>
2025-05-21 12:59:57 +0530

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = var.public_key
+  public_key = file("~/.ssh/deployer-key.pub")
 }
 
 resource "aws_instance" "web" {
